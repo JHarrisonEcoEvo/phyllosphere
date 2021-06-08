bfc43e40b6a817caaefc49e0ede91afd2798e5d4
Josh Harrison <jharrison@morgan.uwyo.edu>
2021-06-08 11:46:36 -0600

    fc

---
+++ scripts/wrangling_Daubenmire_data.R
@@ -0,0 +1,104 @@
+rm(list=ls())
+library(tidyr)
+library(tibble)
+library(dplyr)
+
+dat <- read.csv("./raw_data_notIncluding_sequenceData/rawODKdata/alldata_ODK_combined.csv",
+                stringsAsFactors = F, 
+                fill = T)
+#checking that the number of sites present is as expected
+length(unique(dat$siteLabel))
+
+#Goal is a data.frame with site, Daub plot, taxon and abundance class.
+
+#Order of operations:
+#1. fill empty rows (currently many blanks)
+#2. Daubemire plots need unique id (we get this from lat long data)
+#3. Convert cover classes to values between 0-100
+#Cover classes are: 0 (0%) 1 (0.1–5%), 2 (5–25%), 3 (25–50%), 4 (50–75%), 5 (75–95%), and 6 (>95%)
+#4. Calculate Shannon's entropy for the veg. cover data for each site. This may be a bit tricky.
+#6. Export the Shannon's entropy to processed data, then bring it in to the metadata
+#during the metadata wrangling scripts.
+
+#1-2. Fill rows (#2 is accomplished here as well)
+dat <- as_tibble(dat)
+
+#have to change empty strings to NAs prior to fill
+dat[dat == ""] <- NA
+
+dat <- fill(dat,2:length(dat), .direction="down")
+
+#3. 
+dat$daubPlots__VegCompositionBlock__coverClass <- recode(as.character(dat$daubPlots__VegCompositionBlock__coverClass), 
+       "1"="0.1",
+       "2"="5",
+       "3"="25",
+       "4"="50",
+       "5"="75",
+       "6"="95",
+       )
+
+dat$daubPlots__daubplotbare <- recode(as.character(dat$daubPlots__daubplotbare), 
+                                                         "1"="0.1",
+                                                         "2"="5",
+                                                         "3"="25",
+                                                         "4"="50",
+                                                         "5"="75",
+                                                         "6"="95",
+)
+dat$daubPlots__daubplotltter <- recode(as.character(dat$daubPlots__daubplotltter), 
+                                      "1"="0.1",
+                                      "2"="5",
+                                      "3"="25",
+                                      "4"="50",
+                                      "5"="75",
+                                      "6"="95",
+)
+dat$daubPlots__daubplotgraminoid <- recode(as.character(dat$daubPlots__daubplotgraminoid), 
+                                       "1"="0.1",
+                                       "2"="5",
+                                       "3"="25",
+                                       "4"="50",
+                                       "5"="75",
+                                       "6"="95",
+)
+dat$daubPlots__daubplotforb <- recode(as.character(dat$daubPlots__daubplotforb), 
+                                       "1"="0.1",
+                                       "2"="5",
+                                       "3"="25",
+                                       "4"="50",
+                                       "5"="75",
+                                       "6"="95",
+)
+dat$daubPlots__daubplotshrub <- recode(as.character(dat$daubPlots__daubplotshrub), 
+                                       "1"="0.1",
+                                       "2"="5",
+                                       "3"="25",
+                                       "4"="50",
+                                       "5"="75",
+                                       "6"="95",
+)
+dat$daubPlots__daubplottree <- recode(as.character(dat$daubPlots__daubplottree), 
+                                       "1"="0.1",
+                                       "2"="5",
+                                       "3"="25",
+                                       "4"="50",
+                                       "5"="75",
+                                       "6"="95",
+)
+
+#4. Need to convert cover measurements to total percentage for the whole site
+#Could combine data for all identical taxa within a site..
+#Could convert cover measurements to rows so, four rows for each site, with x columns, for taxa
+#then calc shannons for each row and take average shannons
+
+#remove NAs in the species code
+dat <- dat[!is.na(dat$daubPlots__VegCompositionBlock__speciesCode),]
+
+newdat <- as_tibble(data.frame(dat$siteLabel,
+                     dat$gpsLocation.Latitude,
+                     dat$daubPlots__VegCompositionBlock__speciesCode,
+                     dat$daubPlots__VegCompositionBlock__coverClass))
+
+pivot_wider(names_from = as.character(newdat$dat.daubPlots__VegCompositionBlock__speciesCode),
+       values_from = as.numeric(newdat$dat.daubPlots__VegCompositionBlock__coverClass))
