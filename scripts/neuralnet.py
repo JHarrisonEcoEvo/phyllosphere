59166853a0bac1f0b40faab215d5e2ae745fee15
Josh Harrison <jharrison@mac.uwyo.edu>
2021-06-15 16:33:41 -0600

    progress on feature selection. working on one hot

--- scripts/neuralnet.py
+++ scripts/neuralnet.py
@@ -1,36 +1,154 @@
+#Neural net analysis of phyllosphere data
 #Code adapted from https://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/
+#Contents of code:
+#Loading and examining data 
+#Feature selection
+#Feature engineering: dummy variable creation, NA imputation, scaling/centering
+#Model definition and compilation
+#Checking model performance (standard test/train split)
 
 import pandas as pd
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+# from keras.wrappers.scikit_learn import KerasRegressor
+# from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import KFold
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.pipeline import Pipeline
 
-data = pd.read_csv('processedData/ITSmetadat_wrangled_for_post_modeling_analysis.csv')
+#Load data
+data = pd.read_csv('../processedData/ITSmetadat_wrangled_for_post_modeling_analysis.csv')
 
-data.head()
+#Explore the data
+data.head
+data.info()
+data.describe() #This is like R's summary command when called on numerical data
+data['taxon.x'].value_counts()
+#data.columns
 
-#Select the columns that I want to use as features in the net
-cols=["taxon.x","area_cm2","mass_extracted_g","circumStem","width","plantMeasurements__width2","plant_vol","plantMeasurements__height","height_sample","Ambient_Humidity","Ambient_Temperature","Leaf_Temp_Differential","LEF","Light_Intensity..PAR.","NPQt","Phi2","PhiNO","PhiNPQ","Relative_Chlorophyll","thickness","absorbance_420","absorbance_530","absorbance_605","absorbance_650","absorbance_730","absorbance_850","absorbance_880","absorbance_940","contactless_temp","ecs_initial","ecs_max","ecs_r_squared","FmPrime","FoPrime","Fs","FvP.FmP","G","gH.","NPQt_MPF","PhiNPQ","pressure","qL","R","Rel_Chl_intensity","RFd","SPAD_420","SPAD_420_intensity","SPAD_530","SPAD_530_intensity","SPAD_605","SPAD_605_intensity","SPAD_650","SPAD_650_intensity","SPAD_730","SPAD_730_intensity","SPAD_850","SPAD_850_intensity","SPAD_880","SPAD_880_intensity","spad_raw1","spad_raw2","spad_raw3","TimeofDay","Latitude","Longitude","waterRetention","toughness","elev_m","slope_perc","treeRich","shrubRich","deadDown","latitude","altitude","longitude","densitometer.y","shannons_flora.y","MEM1.y","MEM2.y","julianDate"]
+data['sla'] = data['area_cm2'] / data['mass_g']
+
+#Handy code for making histograms
+# import matplotlib.pyplot as plt
+# data.hist(bins=50,figsize=(20,15))
+# plt.show()
+
+#Select the columns that I want to use as features
+#Avoiding things that are likely duplicative or boring
+#TIP: Note the use of slashes here to improve readability, and allow me to 
+#quickly remove certain features.
+
+#I am removing those categorical features that need dummy coding
+cols=["area_cm2",\
+    "absorbance_420",\
+    "absorbance_940",\
+    "Ambient_Humidity",\
+    "Ambient_Temperature",\
+    "B",\
+    "circumStem",\
+    #"compartment.x",\
+    "contactless_temp",\
+    "deadDown",\
+    "densitometer.y",\
+    "ecs_initial",\
+    "ecs_max",
+    "elev_m",\
+    "FmPrime",\
+    "FoPrime",\
+    "Fs",\
+    "FvP.FmP",\
+    "G",\
+    "gH.",\
+    "height_sample",\
+    "julianDate",\
+    "Latitude",\
+    "Leaf_Temp_Differential",\
+    "LEF",\
+    "leaves_extracted",\
+    "Light_Intensity..PAR.",\
+   # "lifehistory",\
+    "Longitude",\
+    "mass_extracted_g",\
+    "MEM1.y",\
+    "MEM2.y",\
+    "NPQt_MPF",\
+   # "phenology",\
+    "Phi2",\
+    "PhiNO",\
+    "PhiNPQ",\
+    "PhiNPQ",\
+    "plant_vol",\
+    "pressure",\
+    "qL",\
+    "R",\
+    "Rel_Chl_intensity",\
+    "Relative_Chlorophyll",\
+    "RFd",\
+    "shannons_flora.y",\
+    "shrubRich",\
+    "sidePlantSampled",\
+    "sla",\
+    "slope_perc",\
+    "SPAD_420_intensity",\
+    "SPAD_420",\
+    "thickness",\
+    "TimeofDay",\
+   #"taxon.x",\
+    "toughness",\
+    "treeRich",\
+    "waterRetention"\
+    # "NPQt",\
+    ]
 
 # split into input (X) and output (Y) variables
 X = data[cols]
 Y = data['shannonsISD']
 
-#Make dummy variables (one hot encoding) for all non-numeric variables in the data
-taxa = pd.get_dummies(X['taxon.x'])
-X = [X, taxa]
-X = pd.concat(X, axis=1)
+#IMPORTANT NOTE: all features need to be numeric, so some sort of encoding
+#is needed. Beware of encoding that could be construed as ordinal. This is why
+#I do dummy (one hot) encoding.
+taxa = pd.get_dummies(data['taxon.x'])
+habit = pd.get_dummies(data['lifehistory'])
+compartment = pd.get_dummies(data['compartment.x'])
+phenology = pd.get_dummies(data['phenology'])
 
-#lots of NULLs
-#print(X[X.isnull().any(axis=1)])
+#Handy way to concatenate data frames, axis decides if fields or rows are added
+#can do pretty smart merging, see help.
+X = pd.concat([X, taxa, habit, compartment, phenology], axis=1)
 
-# define the keras model
-	# create model
+Need to scale and center and do NAs
+
+#####################
+#Do train/test split#
+#####################
+#Code from Geron. 
+#Set seed to get same split. Doing via instantiation of a new pseudorandom
+#number generator, instead of changing global seed. Does not matter in this
+#case, but this is best practice so you know what rng used for what part of 
+#code and will play well with other functions that change seed
+
+np.random.default_rng(666)
+
+def split_train_test(data, test_ratio):
+        shuffled_indices = np.random.permutation(len(data))
+        test_set_size = int(len(data) * test_ratio)
+        test_indices = shuffled_indices[:test_set_size]
+        train_indices = shuffled_indices[test_set_size:]
+        return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_train_test(X, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
+
+#Incidentally, to do this via scikit learn use this
+#from sklearn.model_selection import train_test_split
+#train_set, test_set = train_test_split(data, test_size=0.2, random_state=(666))
+
+############
+# Analysis #
+############
+
+#Define the keras model
 model = Sequential()
 model.add(Dense(1,input_dim=len(X['area_cm2']),kernel_initializer='normal',activation='relu'))
 model.add(Dense(1, kernel_initializer='normal'))
@@ -40,4 +158,3 @@ testX = np.asarray(X["area_cm2"]).astype('float32')
 testY = np.asarray(Y).astype('float32')
 
 model.fit(testX, testY, epochs=20)
-
