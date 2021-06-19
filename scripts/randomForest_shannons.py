24851f0fc0df9e8493fd138ac721e8f31441aa7f
Josh Harrison <jharrison@mac.uwyo.edu>
2021-06-19 13:21:28 -0600

    improved plotting of SHAP values and did some feature selection and reran model.

--- scripts/randomForest_shannons.py
+++ scripts/randomForest_shannons.py
@@ -14,16 +14,7 @@ from sklearn.model_selection import RepeatedKFold
 from sklearn.ensemble import RandomForestRegressor
 
 X = pd.read_csv("./processedData/imputed_scaled_ITS_metadata.csv")
-
-num_features = []
-categorical_features = []
-
-for i in X:
-    if X[i].dtype.kind in 'iufc':
-        num_features.extend([i])
-    else:
-        categorical_features.extend([i])
-        
+ 
 ############
 # QC step ##
 ############
@@ -96,3 +87,95 @@ n_scores = cross_val_score(model, X_array[:,10:], Y_array,
                            error_score='raise')
 # report performance
 print('MAE: %.3f (%.3f)' % (np.mean(n_scores), np.std(n_scores)))
+
+##############
+#SHAP values
+############
+
+import shap 
+shap.initjs()
+
+# Create Tree Explainer object that can calculate shap values
+explainer = shap.TreeExplainer(model)
+
+shap_values = explainer.shap_values(X = X_array[:,10:])
+list_of_labels = list(strat_train_set.loc[:, strat_train_set.columns != 'shannonsISD'].columns)
+feature_names=list_of_labels[10:]
+
+shap.summary_plot(shap_values, 
+                  X_array[:,10:],
+                  feature_names=feature_names)
+
+#Extract SHAP values and then remove some features and try rerunning model
+shapValues = pd.DataFrame(shap_values, columns=feature_names, 
+                          index = strat_train_set.loc[:,'samplename'])
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+duds = shapValues.sum(axis = 0)[abs(shapValues.sum(axis = 0)) < 0.2]
+
+[duds.index]
+
+import re
+l = duds.index.tolist()
+r = re.compile(r'.*\s+.*')
+newlist = list(filter(r.match, l))
+#print(newlist)
+
+#Remove taxa
+for i in newlist:
+    l.remove(i)
+
+#Now we have a list, l, that has features to be removed. Lets remove them and rerun the model 
+###########################################
+# Running model again after removing useless features #
+###########################################
+
+X = pd.read_csv("./processedData/imputed_scaled_ITS_metadata.csv")
+
+#Remove features that we have deemed not helpful
+for i in l:
+    X.drop(i, inplace=True, axis=1)
+
+#Do train/test split#
+split = StratifiedShuffleSplit(n_splits=1, test_size=(0.2), random_state=(666))
+for train_index, test_index in split.split(X, X["EN"]):
+    strat_train_set = X.loc[train_index]
+    strat_test_set = X.loc[test_index]
+
+strat_test_set.shape  
+strat_train_set.shape
+
+X_array = np.array(strat_train_set.loc[:, strat_train_set.columns != 'shannonsISD'])
+Y_array = np.array(strat_train_set['shannonsISD']).reshape(-1,1)
+
+Xtest_array = np.array(strat_test_set.loc[:, strat_test_set.columns != 'shannonsISD'])
+Ytest_array = np.array(strat_test_set['shannonsISD']).reshape(-1,1)
+
+# define the model
+model = RandomForestRegressor(n_estimators=200,
+                              criterion = 'mse',
+                              min_samples_split = 2,
+                              )
+model.fit(X=X_array[:,10:], y=Y_array.ravel())
+
+yhat = model.predict(Xtest_array[:,10:])
+
+r2 = r2_score(Ytest_array,yhat)
+
+pearsonr(np.squeeze(Ytest_array), np.squeeze(yhat))
+
+import matplotlib.pyplot as plt
+plt.scatter(Ytest_array, yhat, )
+ 
+plt.xlabel("Expected values")
+plt.ylabel("Observed values");
+
+
+t = plt.text(0.15, 0.5,
+             "R2 = {}%".format(round(r2*100)),
+             weight='bold')
+t.set_bbox(dict(facecolor='red', alpha=0.8, edgecolor='red'))
+
+
+
