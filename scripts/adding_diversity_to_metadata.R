3bceb9a63a8ccf9cd7159e5f212d9bd31c98f918
J. G. Harrison <jgharrison1982@gmail.com>
2021-08-10 16:39:10 -0600

    housecleaning

--- scripts/adding_diversity_to_metadata.R
+++ scripts/adding_diversity_to_metadata.R
@@ -2,19 +2,17 @@ rm(list=ls())
 
 #Bring in data, packages, set up color scheme
 its <- read.csv("./processedData/ITSp_estimates_wrangled_for_post_modeling_analysis_divided_by_ISD.csv")
-its_nonISd <- read.csv("./processedData/ITS_multinomial_p_estimates.csv")
+#its_nonISd <- read.csv("./processedData/ITS_multinomial_p_estimates.csv")
 meta <- read.csv("./processedData/ITSmetadat_wrangled_for_post_modeling_analysis.csv", stringsAsFactors = F, header = T)
 
 dim(its)
 dim(meta)
 
-meta$shannons <- NA
-meta$simpsons <- NA
 for(i in 1:dim(its)[1]){
-  meta$shannonsISD[i] <- exp(vegan::diversity(its[i,5:(length(its)-2)], index = "shannon"))
-  meta$simpsonsISD[i] <- 1/(vegan::diversity(its[i,5:(length(its)-2)], index = "simpson"))
-  meta$shannons[i] <- exp(vegan::diversity(its_nonISd[i,5:(length(its_nonISd)-2)], index = "shannon"))
-  meta$simpsons[i] <- 1/(vegan::diversity(its_nonISd[i,5:(length(its_nonISd)-2)], index = "simpson"))
+  meta$shannonsISD[i] <- exp(vegan::diversity(its[i,grep("Zotu", names(its))], index = "shannon"))
+  meta$simpsonsISD[i] <- 1/(vegan::diversity(its[i,grep("Zotu", names(its))], index = "simpson"))
+ # meta$shannons[i] <- exp(vegan::diversity(its_nonISd[i,grep("Zotu", names(its_nonISd))], index = "shannon"))
+ # meta$simpsons[i] <- 1/(vegan::diversity(its_nonISd[i,grep("Zotu", names(its_nonISd))], index = "simpson"))
 }
 write.csv(meta, file = "./processedData/ITSmetadat_wrangled_for_post_modeling_analysis.csv",row.names = F)
 
@@ -27,18 +25,13 @@ rm(list=ls())
 
 #Bring in data, packages, set up color scheme
 sixs <- read.csv("./processedData/16sp_estimates_wrangled_for_post_modeling_analysis_divided_by_ISD.csv")
-sixs_nonISd <- read.csv("./processedData/16sp_estimates_wrangled_for_post_modeling_analysis.csv")
+#sixs_nonISd <- read.csv("./processedData/16sp_estimates_wrangled_for_post_modeling_analysis.csv")
 meta <- read.csv("./processedData/16smetadat_wrangled_for_post_modeling_analysis.csv", stringsAsFactors = F, header = T)
 
-dim(sixs)
-dim(meta)
-
-meta$shannons <- NA
-meta$simpsons <- NA
 for(i in 1:dim(sixs)[1]){
-  meta$shannonsISD[i] <- exp(vegan::diversity(sixs[i,5:(length(sixs)-2)], index = "shannon"))
-  meta$simpsonsISD[i] <- 1/(vegan::diversity(sixs[i,5:(length(sixs)-2)], index = "simpson"))
-  meta$shannons[i] <- exp(vegan::diversity(sixs_nonISd[i,5:(length(sixs_nonISd)-2)], index = "shannon"))
-  meta$simpsons[i] <- 1/(vegan::diversity(sixs_nonISd[i,5:(length(sixs_nonISd)-2)], index = "simpson"))
+  meta$shannonsISD[i] <- exp(vegan::diversity(sixs[i,grep("Zotu", names(sixs))], index = "shannon"))
+  meta$simpsonsISD[i] <- 1/(vegan::diversity(sixs[i,grep("Zotu", names(sixs))], index = "simpson"))
+#  meta$shannons[i] <- exp(vegan::diversity(sixs_nonISd[i,grep("Zotu", names(sixs_nonISd))], index = "shannon"))
+#  meta$simpsons[i] <- 1/(vegan::diversity(sixs_nonISd[i,grep("Zotu", names(sixs_nonISd))], index = "simpson"))
 }
 write.csv(meta, file = "./processedData/16Smetadat_wrangled_for_post_modeling_analysis.csv",row.names = F)
