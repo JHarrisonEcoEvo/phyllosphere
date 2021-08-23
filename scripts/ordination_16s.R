c2ff07e02b2c521d5e9408c1b4b1d910e530a6a7
J. G. Harrison <jgharrison1982@gmail.com>
2021-08-23 13:50:30 -0600

    adding stuff to redo with count data as a sanity check

--- scripts/ordination_16s.R
+++ scripts/ordination_16s.R
@@ -2,6 +2,10 @@ rm(list = ls())
 library(ecodist)
 dat <- read.csv("./processedData/16sp_estimates_wrangled_for_post_modeling_analysis_divided_by_ISD.csv",
                 stringsAsFactors = F)
+# dat <- read.csv("./processedData/otuTables/smallmem97_16S_for_modeling_rearranged_for_CNVRG",
+#                 stringsAsFactors = F)
+#datr <-  vegan::drarefy(dat[,3:length(dat)], 2300)
+
 dat[1:3,1:5]
 tail(names(dat))
 dim(dat)
@@ -23,6 +27,7 @@ names(dat)[length(dat)-4]
 #dat_bc <- bcdist(normDat)
 
 dat_bc <- bcdist(dat[,4:(length(dat)-4)])
+#dat_bc <- bcdist(datr[,3:2360])
 
 #dat_e <- dist(normDat,method = "euclidean")
 
